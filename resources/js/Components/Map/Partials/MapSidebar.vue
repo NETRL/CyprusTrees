4b6136a6f77a4e5b0947586d1c917617808734e8
Tasoskourouniadis <tasoskourouniadis@gmail.com>
2025-11-27 16:03:16 +0200

    minor

--- resources/js/Components/Map/Partials/MapSidebar.vue
+++ resources/js/Components/Map/Partials/MapSidebar.vue
@@ -1,6 +1,6 @@
 <template>
     <aside :class="[
-        'max-lg:hidden absolute left-0 top-0 m-4 rounded-lg bg-white/90 backdrop-blur-xs dark:bg-gray-900/90 border border-gray-200 dark:border-gray-800 shadow-lg transition-all duration-300 ease-in-out z-50',
+        'max-lg:hidden absolute left-0 top-0 m-4 rounded-lg bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 shadow-lg transition-all duration-300 ease-in-out z-50',
         'max-h-[calc(100vh-7rem)] overflow-hidden w-[390px] flex flex-col',
         {
             'opacity-100 px-5 py-5 pointer-events-auto': isExpanded || isHovered,
